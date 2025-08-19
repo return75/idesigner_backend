d9a8e2dff65b730ad891a7eca52f2bb26d976d7c
Dev5 <dev5@webteamwork.ca>
2025-08-19 22:59:24 +0430

    fix user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->unique()->phoneNumber(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+          //  'remember_token' => Str::random(10),
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,19 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        User::factory(10)->create();
         User::factory()->create([
-            'name' => 'Alireza',
+            'first_name' => 'Alireza',
+            'last_name' => 'Atashfaraz',
             'email' => 'atashfaraz8@gmail.com',
         ]);
-
-        DB::table('sizes')->insert([
-            ['code' => 'XS', 'name_en' => 'Extra Small', 'name_fa' => 'خیلی کوچک'],
-            ['code' => 'S',  'name_en' => 'Small',       'name_fa' => 'کوچک'],
-            ['code' => 'M',  'name_en' => 'Medium',      'name_fa' => 'متوسط'],
-            ['code' => 'L',  'name_en' => 'Large',       'name_fa' => 'بزرگ'],
-            ['code' => 'XL', 'name_en' => 'Extra Large', 'name_fa' => 'خیلی بزرگ'],
+        $this->call([
+            SizeSeeder::class,
         ]);
+
     }
 }
